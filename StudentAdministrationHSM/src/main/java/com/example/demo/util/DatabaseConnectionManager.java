fa2eb4dd2fddb640a7fb2c57243dcb76ebb8c65e
omar0190 <61865216+omar0190@users.noreply.github.com>
2020-05-04 18:26:04 +0200

    lulu4

--- StudentAdministrationHSM/src/main/java/com/example/demo/util/DatabaseConnectionManager.java
+++ StudentAdministrationHSM/src/main/java/com/example/demo/util/DatabaseConnectionManager.java
@@ -1,6 +1,13 @@
 package com.example.demo.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnectionManager {
     private String url;
@@ -12,5 +19,29 @@ public class DatabaseConnectionManager {
     }
 
     public Connection getConnection(){
+        Properties prop = new Properties();
+        try{
+            FileInputStream propFile = new FileInputStream("src/main/resources/application.properties");
+            prop.load(propFile);
+            url = prop.getProperty("db.url");
+            user = prop.getProperty("db.user");
+            password = prop.getProperty("db.password");
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try{
+            conn = DriverManager.getConnection(url, user, password);
+
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            e.getErrorCode();
+        }
+        return conn;
     }
 }
