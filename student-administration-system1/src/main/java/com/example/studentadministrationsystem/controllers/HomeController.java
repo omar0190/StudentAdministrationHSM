4ee24b0ef5711b398f748b4a9087ce6d09a28f9d
alix0731 <56973809+alix0731@users.noreply.github.com>
2020-05-05 17:07:59 +0200

    ggg

--- student-administration-system1/src/main/java/com/example/studentadministrationsystem/controllers/HomeController.java
+++ student-administration-system1/src/main/java/com/example/studentadministrationsystem/controllers/HomeController.java
@@ -5,6 +5,10 @@ import com.example.studentadministrationsystem.repositories.StudentRepositoryDB;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @Controller
 public class HomeController {
 
@@ -12,7 +16,11 @@ public class HomeController {
     IStudentRepository iStudentRepository;
 
     public HomeController() {
-        iStudentRepository = new StudentRepositoryDB();
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql//localhost:3306/my_company" , "admin" , "Ali010298");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @GetMapping("/")
